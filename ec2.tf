47b3a1663ca0ba62b08affe6b2fe579ae1c98dc9
Lynda Nwabudike <lynda.holivia@gmail.com>
2024-11-20 03:20:21 +0000

    commiting change

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,16 @@
-resource "aws_instance" "airbnb-web" {
+resource "aws_instance" "dev-airbnb-web" {
   ami           =var.ami_id
   instance_type = var.instance_type
 
   tags = {
     Name = "airbnb-web-server"
+  }
+}
+resource "aws_instance" "prod-airbnb-web" {
+  ami           =var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "airbnb-web-server2"
   }
 }
